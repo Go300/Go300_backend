5c8e7e33a0af5b259d4829d474227e8d013c2ae1
muslimbeibytuly <muslimbeibytuly@gmail.com>
2018-09-01 20:51:53 +0600

    Add everything, but not push notifications

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3-alpine
 ENV PYTHONUNBUFFERED=1
 
 RUN apk add --no-cache linux-headers bash gcc \
-    zlib-dev libmagic make libpq postgresql-dev musl-dev libxslt-dev libxml2-dev tzdata
+    zlib-dev libmagic make libpq postgresql-dev musl-dev libxslt-dev libxml2-dev tzdata libffi-dev
 
 RUN rm -rf /src
 COPY ./src /src
--- src/core/celery.py
+++ src/core/celery.py
@@ -4,30 +4,48 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-from app.models import Subscription
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('core')
 
-app.config_from_object('core.settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    for hour in range(0, 11):
-        for minute in (0, 30):
-            sender.add_periodic_task(
-                crontab(hour=hour, minute=minute),
-                notify_users.s(hour, minute=(minute + 30))
-            )
-
-
 @app.task
 def notify_users(hour, minute):
-    when = '{0}:{1}'.format(str(hour), str(minute))
-    subscriptions = Subscription.objects.filter(when=when).all()
-    for subscription in subscriptions:
-        print(subscription.member.username)
+    print('{0}:{1}'.format(str(hour), str(minute)))
+    # when = '{0}:{1}'.format(str(hour), str(minute))
+    # subscriptions = Subscription.objects.filter(when=when).all()
+    # for subscription in subscriptions:
+    #     print(subscription.member.username)
+    # print('subscriptions: {}'.format(subscriptions.count()))
+
+
+tasks = {}
+
+for hour in range(0, 24):
+    for minute in (0, 30):
+        tasks['notify users_{0}_{1}'.format(hour, minute)] = {
+            'task': 'core.celery.notify_users',
+            'schedule': crontab(hour=str(hour), minute=str(minute)),
+            'args': ((hour + (minute + 30) // 60) % 24, (minute + 30) % 60)
+        }
+
+app.conf.beat_schedule = tasks
+
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(**kwargs):
+#     app.add_periodic_task(
+#         notify_users.s(hour=1, minute=(1 + 30)),
+#         run_every=crontab(hour='*', minute='*'),
+#         name='notify users'
+#     )
+# for hour in range(0, 23):
+#     for minute in (0, 30):
+#         sender.add_periodic_task(
+#             crontab(hour=str(hour), minute=str(minute)),
+#             notify_users.s(hour, minute=(minute + 30)),
+#             name='notify users'
+#         )

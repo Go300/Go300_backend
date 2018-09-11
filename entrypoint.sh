6b4f173d2335e28df80ee8a5e6dbb0be9dc1b237
muslimbeibytuly <muslimbeibytuly@gmail.com>
2018-09-12 01:13:57 +0600

    entrypoint.sh to source

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 5s
-python manage.py migrate
-python manage.py makemigrations proj
-python manage.py migrate
-python manage.py makemigrations
-python manage.py migrate
-python manage.py migrate && python manage.py runserver 0.0.0.0:8000
